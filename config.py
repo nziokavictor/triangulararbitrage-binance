b0e9d583a1c9691fcda137f45881904e1f878dba
nzioka <victor.nzioka@mpesafoundationacademy.ac.ke>
2026-06-27 01:55:04 +0300

    Clarify Telegram bot comment in config.py
    Updated comment to clarify the purpose of the Telegram bot.

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+#the apikey and secret key below are of binance testnet that allows you to use virtual money.
+# to obtain yo api key and api secret go to binancetesnet.com 
+
 API_KEY = "tJF8Y2XsZ1LGdqF4KM0D0inCO8kLVmzbvYikKjIMWGVR7MDYceo8BlLmpaVDmHql"
 API_SECRET = "YzB8NEFb3bVNBlb0eMTqgHbQdBJKWKtpjXtne34enrpHYBzlbvW1wPGVgR7dHGTO"
 
@@ -11,5 +14,6 @@ TRADE_FEE = 0.001  # 0.1%  # Fee per trade (Binance standard)
 MAX_TRADE_USDT = 100
 
 #telegram bot 
+#this is keys to a telegram bot i created. the telegram bot is to notify me of any activity
 TELEGRAM_TOKEN = "8970879593:AAHilALYkrXsuSQ-BqIUz2RdkSze_A6PuBo"
 TELEGRAM_CHAT_ID = "5552946379"
